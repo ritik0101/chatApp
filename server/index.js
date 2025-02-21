850d2e479ff7d3dad7722309311fc0f5f516c000
Ritik Limje <121362640+ritik0101@users.noreply.github.com>
2025-02-22 00:04:27 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,18 +2,32 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
+const path = require('path');
 const { Server } = require("socket.io");
 
 app.use(cors());
+
+const port = process.env.PORT || 5000;
+
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // Ensure this matches your frontend URL
+    origin: "*", // Ensure this matches your frontend URL
     methods: ["GET", "POST"],
   },
 });
+
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve the main HTML file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
